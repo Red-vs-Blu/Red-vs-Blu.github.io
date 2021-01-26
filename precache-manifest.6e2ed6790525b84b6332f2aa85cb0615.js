d0d7076e41dbf2d339c5c9a50ee787b64cd38482
daototpyrc <daototpyrc@gmail.com>
2021-01-26 14:39:05 -0800

    Updates

---
+++ precache-manifest.6e2ed6790525b84b6332f2aa85cb0615.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63dbeb6f973ae50c113ce397df6877b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07dc4ebf9d5706150996",
+    "url": "/static/css/main.7704f3df.chunk.css"
+  },
+  {
+    "revision": "9fbf72c0685ee8b2c988",
+    "url": "/static/js/2.af9df350.chunk.js"
+  },
+  {
+    "revision": "f615ca4c03acfc767f242979865a0ad1",
+    "url": "/static/js/2.af9df350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07dc4ebf9d5706150996",
+    "url": "/static/js/main.e17d7044.chunk.js"
+  },
+  {
+    "revision": "a3001d5fe6d099647b70",
+    "url": "/static/js/runtime-main.862be350.js"
+  },
+  {
+    "revision": "2db009d884b526dbd3ccd985cb52de25",
+    "url": "/static/media/regular.2db009d8.ttf"
+  }
+]);
